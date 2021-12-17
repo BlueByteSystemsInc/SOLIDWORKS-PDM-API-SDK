e9ca388cb3c9a08ed8419675011ab191e842d563
Amen Jlili (Home) <amen@bluebyte.biz>
2021-12-17 15:31:28 -0800

    #22 added constructor and enums

--- src/BlueByte.SOLIDWORKS.PDMProfessional.PDMAddInFramework/Attributes/RequiredVersionAttribute.cs
+++ src/BlueByte.SOLIDWORKS.PDMProfessional.PDMAddInFramework/Attributes/RequiredVersionAttribute.cs
@@ -2,15 +2,48 @@
 
 namespace BlueByte.SOLIDWORKS.PDMProfessional.PDMAddInFramework.Attributes
 {
+    /// <summary>
+    /// Minium supported PDM Version.
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
     public class RequiredVersionAttribute : Attribute
     {
-         
+
+        /// <summary>
+        /// Gets or sets the major.
+        /// </summary>
+        /// <value>
+        /// The major.
+        /// </value>
         public int Major { get; set; }
+        /// <summary>
+        /// Gets or sets the minor.
+        /// </summary>
+        /// <value>
+        /// The minor.
+        /// </value>
         public int Minor { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequiredVersionAttribute"/> class.
+        /// </summary>
+        /// <param name="major">The major.</param>
+        /// <param name="minor">The minor.</param>
         public RequiredVersionAttribute(int major, int minor)
         {
             Major = major;
             Minor = minor;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequiredVersionAttribute"/> class.
+        /// </summary>
+        /// <param name="PDMYear">The PDM year.</param>
+        /// <param name="ServicePack">The service pack.</param>
+        public RequiredVersionAttribute(Enums.Year_e PDMYear, Enums.ServicePack_e ServicePack)
+        {
+            Major = (int)PDMYear;
+            Minor = (int)ServicePack;
         }
     }
 }
---
+++ src/BlueByte.SOLIDWORKS.PDMProfessional.PDMAddInFramework/Enums/PDMProfessionalVersion_e.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueByte.SOLIDWORKS.PDMProfessional.PDMAddInFramework.Enums
+{
+    /// <summary>
+    /// PDM Professional year.
+    /// </summary>
+    public enum Year_e
+    {
+        /// <summary>
+        /// PDM Professional 2006.
+        /// </summary>
+        PDM2006 = 6,
+        /// <summary>
+        /// PDM Professional 2007.
+        /// </summary>
+        PDM2007 = 7,
+        /// <summary>
+        /// PDM Professional 2008.
+        /// </summary>
+        PDM2008 = 8,
+        /// <summary>
+        /// PDM Professional 2009.
+        /// </summary>
+        PDM2009 = 9,
+        /// <summary>
+        /// PDM Professional 2010.
+        /// </summary>
+        PDM2010 = 10,
+        /// <summary>
+        /// PDM Professional 2011.
+        /// </summary>
+        PDM2011 = 11,
+        /// <summary>
+        /// PDM Professional 2012.
+        /// </summary>
+        PDM2012 = 12,
+        /// <summary>
+        /// PDM Professional 2013.
+        /// </summary>
+        PDM2013 = 13,
+        /// <summary>
+        /// PDM Professional 2014.
+        /// </summary>
+        PDM2014 = 14,
+        /// <summary>
+        /// PDM Professional 2015.
+        /// </summary>
+        PDM2015 = 15,
+        /// <summary>
+        /// PDM Professional 2016.
+        /// </summary>
+
+        PDM2016 = 16,
+        /// <summary>
+        /// PDM Professional 2017.
+        /// </summary>
+        PDM2017 = 17,
+        /// <summary>
+        /// PDM Professional 2018.
+        /// </summary>
+        PDM2018 = 18,
+        /// <summary>
+        /// PDM Professional 2019.
+        /// </summary>
+        PDM2019 = 19,
+        /// <summary>
+        /// PDM Professional 2020.
+        /// </summary>
+        PDM2020 = 20,
+        /// <summary>
+        /// PDM Professional 2021.
+        /// </summary>
+        PDM2021 = 21
+
+
+
+    }
+    /// <summary>
+    /// PDM Professional service. Some service pack for specific year may not exist. Please check <see href="https://help.solidworks.com/2021/English/api/epdmapi/ReleaseNotes-epdmapi.html">this link</see> for the release notes.
+    /// </summary>
+    public enum ServicePack_e
+    {
+        /// <summary>
+        /// Service pack.
+        /// </summary>
+        SP0 = 0,
+
+        /// <summary>
+        /// Service pack.
+        /// </summary>
+        SP1 = 1,
+
+        /// <summary>
+        /// Service pack.
+        /// </summary>
+        SP2 = 2,
+
+        /// <summary>
+        /// Service pack.
+        /// </summary>
+        SP3 = 3,
+
+        /// <summary>
+        /// Service pack.
+        /// </summary>
+        SP4 = 4,
+
+        /// <summary>
+        /// Service pack.
+        /// </summary>
+        SP5 = 5,
+        
+    }
+}
