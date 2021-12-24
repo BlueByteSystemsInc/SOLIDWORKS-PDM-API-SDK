edd620bce8d21d89576c5c3463058d95a4cce8ba
Andrey Arekhva <a.arekhva@gmail.com>
2021-12-24 13:49:11 +0300

    #24 Add a UI wizard to specify add-in info

---
+++ VSTemplate/ProjectTemplateWizard/WizardWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ProjectTemplateWizard
+{
+    /// <summary>
+    /// Interaction logic for WizardWindow.xaml
+    /// </summary>
+    public partial class WizardWindow : Window
+    {
+        public AddInInfo AddInInfo { get; set; } = new AddInInfo();
+
+        public WizardWindow()
+        {
+            InitializeComponent();
+            DataContext = AddInInfo;
+        }
+
+        private void OnOkButtonClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
