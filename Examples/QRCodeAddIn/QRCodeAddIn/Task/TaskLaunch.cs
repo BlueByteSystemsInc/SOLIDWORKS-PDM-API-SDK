de738885a4c549baa95bb0d0997fb1dd90a9bf27
Amen Jlili (Home) <amen@bluebyte.biz>
2022-01-01 15:08:31 -0800

    Readded Example

---
+++ Examples/QRCodeAddIn/QRCodeAddIn/Task/TaskLaunch.cs
@@ -0,0 +1,13 @@
+﻿using EPDM.Interop.epdm;
+
+namespace QRCodeAddIn.Task
+{
+    public partial class AddIn
+    {
+
+        public void TaskLaunch(ref EdmCmd cmdData, ref EdmCmdData[] ppoData)
+        {
+
+        }
+    }
+}
