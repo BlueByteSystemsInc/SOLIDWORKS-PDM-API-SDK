8c52a1da59f2dc38e7eacc42e413f71e84726ace
Amen Jlili (Home) <amen@bluebyte.biz>
2022-01-01 17:58:34 -0800

    added logger example

---
+++ Examples/File logger example/QRCodeAddIn/Task/SetupPages/SettingsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRCodeAddIn.Task.SetupPages
+{
+    public class SettingsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This method is called by the Set accessor of each property.  
+        // The CallerMemberName attribute that is applied to the optional propertyName  
+        // parameter causes the property name of the caller to be substituted as an argument.  
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Examples/File logger example/QRCodeAddIn/Task/TaskSetupButton.cs
@@ -0,0 +1,13 @@
+﻿using EPDM.Interop.epdm;
+
+namespace QRCodeAddIn.Task
+{
+    public partial class AddIn
+    {
+
+        public void TaskSetupButton(ref EdmCmd cmdData, ref EdmCmdData[] ppoData)
+        {
+
+        }
+    }
+}
